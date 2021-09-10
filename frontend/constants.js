f2361d9bb8a1b9ec3454ac55401cdfea9e90acf4
Slawomir Stec <slawomirstec@gazeta.pl>
2021-09-10 09:40:44 +0400

    Init commit.

--- frontend/constants.js
+++ frontend/constants.js
@@ -6,8 +6,8 @@ export default {
   isDevMode: (process.env.NODE_ENV !== 'production'),
   API_URL: 'https://api.awss.ws/v1',
   BASE_URL: 'https://api.awss.ws/v1',
-  FACEBOOK_CLIENT_ID: '365798288420194',
+  FACEBOOK_CLIENT_ID: '',
   FACEBOOK_REDIRECT_URL: 'https://api.awss.ws/v1/authenticate/oauth-step1',
-  GOOGLE_CLIENT_ID: '848226878916-recutqmif2g0kt7f5tcpno2jomihsj3q.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '',
   GOOGLE_REDIRECT_URL: 'https://api.awss.ws/v1/authenticate/oauth-step1'
 }
